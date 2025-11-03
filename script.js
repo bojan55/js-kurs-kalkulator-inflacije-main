053bcf76032373811f1c576aef7381e7a3634696
bojan55 <bojan.bajic55@gmail.com>
2025-11-03 20:23:32 +0100

    Create function inflationCalculator.

---
+++ script.js
@@ -0,0 +1,21 @@
+function inflationCalculator(){
+    let inflationRate = document.querySelector('#inflationRate');
+    inflationRate = parseFloat(inflationRate.value);
+    let money = document.querySelector('#money');
+    money = parseFloat(money.value);
+    let years = document.querySelector('#years');
+    years = parseFloat(years.value);
+
+    let worth = money + (money * (inflationRate / 100));
+
+    for(let i = 1; i < years; i++){
+        worth += worth * (inflationRate / 100);
+    }
+
+    worth = worth.toFixed(2);
+
+    let newElement = document.createElement('div');
+    newElement.innerText = `Danasnjih ${money} eura vrijedi isto kao ${worth} eura za ${years} godina!`;
+
+    document.querySelector('.container').appendChild(newElement);
+}
